00d928f7c051842260d5b56fc629d6fa42dfc897
ddurnev <ddurnev@griddynamics.com>
2012-03-29 18:08:06 +0400

    Removed shell scripts from default distribution.

--- distribution/run_client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-VERSION=1.0-SNAPSHOT
-TARGET=./target/genesis-${VERSION}
-java -cp "${TARGET}/*" com.griddynamics.genesis.client.Client $*
-
--- distribution/run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ENV=${1:-nova}
-VERSION=1.0-SNAPSHOT
-TARGET=./target/genesis-${VERSION}
-java -Dbackend.properties=file:${TARGET}/environments/${ENV}.properties -cp "${TARGET}/*" com.griddynamics.genesis.GenesisFrontend
-
