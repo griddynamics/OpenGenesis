1cea58b04f579f64ed5c952d50f48ab62eb9ea2d
Anatoly Vinogradov <avinogradov@griddynamics.com>
2012-07-06 17:18:10 +0400

    [DOC-561][feature] Email notification plugin

---
+++ internal-api/src/main/java/com/griddynamics/genesis/plugin/adapter/AbstractActionResult.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
+ *   http://www.griddynamics.com
+ *
+ *   This library is free software; you can redistribute it and/or modify it under the terms of
+ *   the GNU Lesser General Public License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or any later version.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ *   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   @Project:     Genesis
+ *   @Description: Execution Workflow Engine
+ */
+package com.griddynamics.genesis.plugin.adapter;
+
+import com.griddynamics.genesis.workflow.Action;
+import com.griddynamics.genesis.workflow.ActionResult;
+import com.griddynamics.genesis.workflow.ActionResult$class;
+import scala.Enumeration;
+
+public abstract class AbstractActionResult implements ActionResult {
+
+    private final Action action;
+
+    @Override
+    public Enumeration.Value outcome() {
+        return ActionResult$class.outcome(this);
+    }
+
+    @Override
+    public String desc() {
+        return ActionResult$class.desc(this);
+    }
+
+    /**
+     * Simply delegates to scala-generated "implementation" ActionResult trait.
+     */
+    public void $init$() {
+        ActionResult$class.$init$(this);
+    }
+
+    public AbstractActionResult(Action action) {
+        this.action = action;
+    }
+
+    /**
+     * Scala-style getter.
+     *
+     * @return action
+     */
+    public Action action() {
+        return action;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+}
---
+++ notification/src/main/java/com/griddynamics/genesis/notification/NotificationStep.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
+ *   http://www.griddynamics.com
+ *
+ *   This library is free software; you can redistribute it and/or modify it under the terms of
+ *   the GNU Lesser General Public License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or any later version.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ *   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   @Project:     Genesis
+ *   @Description: E-mail notifications plugin
+ */
+package com.griddynamics.genesis.notification;
+
+import com.griddynamics.genesis.plugin.adapter.AbstractStep;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NotificationStep extends AbstractStep {
+
+    private List<String> emails;
+
+    private String subject;
+
+    private String message;
+
+    public NotificationStep(List<String> emails, String subject, String message) {
+        this.emails = emails;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String stepDescription() {
+        return String.format("Sends email notification to [%s]; subject: %s", StringUtils.join(emails, ","), subject);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationStep{" +
+                "emails=" + emails +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
+}
