2086d74ed0bce9171bf34b18f0a838625252d315
Svyatoslav Reyentenko <rsvato@gmail.com>
2012-07-12 18:05:09 +0400

    [DOC-860][fix], [DOC-862][fix] - Removed frontend rights workarounds. Now all rights are checked on backend only

--- frontend/src/main/scala/com/griddynamics/genesis/rest/WhoamiController.scala
+++ frontend/src/main/scala/com/griddynamics/genesis/rest/WhoamiController.scala
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.{ResponseBody, RequestMethod, Req
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.ServletContext
 import com.griddynamics.genesis.GenesisFrontend
-import org.springframework.beans.factory.annotation.{Value, Autowired}
+import org.springframework.beans.factory.annotation.Autowired
 import com.griddynamics.genesis.users.GenesisRole
 
 @Controller
@@ -38,14 +38,11 @@ class WhoamiController {
   @Autowired
   var servletContext: ServletContext = _
 
-  @Value("${genesis.system.security.groups:ROLE_GENESIS_ADMIN}")
-  var rjaGroups: String = _ //todo (RB) : temp rja fix  to enable UI admin facilities
-
   @RequestMapping(method = Array(RequestMethod.GET))
   @ResponseBody
   def whoami(request: HttpServletRequest): Map[String, Any] = Map(
     "user" -> GenesisRestController.getCurrentUser,
     "logout_disabled" -> "false".equalsIgnoreCase(servletContext.getInitParameter(GenesisFrontend.logoutEnabledParamName)),
-    "administrator" -> (request.isUserInRole(GenesisRole.SystemAdmin.toString) || rjaGroups.split(",").exists(request.isUserInRole(_)))
+    "administrator" -> (request.isUserInRole(GenesisRole.SystemAdmin.toString))
   )
 }
