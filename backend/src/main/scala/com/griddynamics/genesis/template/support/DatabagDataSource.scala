c9a32f19eda4d84c70d2016b6cf2ac10a7baaecc
Svyatoslav Reyentenko <rsvato@gmail.com>
2012-07-17 16:31:18 +0400

    [DOC-XXX] [fix] - Datasources for project data bags

--- backend/src/main/scala/com/griddynamics/genesis/template/support/DatabagDataSource.scala
+++ backend/src/main/scala/com/griddynamics/genesis/template/support/DatabagDataSource.scala
@@ -10,18 +10,25 @@ class DatabagDataSource(repository: DatabagRepository) extends VarDataSource {
   import DatabagDataSource._
 
   var tags: Seq[String] = List()
+  var selector: Option[Int] = None
 
   def getData = {
-    val bags = if (tags.isEmpty) repository.list(None) else repository.findByTags(tags)
+    val bags = if (tags.isEmpty) repository.list(selector) else repository.findByTags(tags, selector)
     bags.map(bag => (bag.name, bag.name)).toMap
   }
 
   def config(map: Map[String, Any]) {
+    val projId = map.get("projectId").map(_.asInstanceOf[Int])
     val tagsList = map.getOrElse(Tags, util.Collections.emptyList())
+    val source = map.getOrElse(Source, "project")
     this.tags = tagsList match {
       case s: String => List(s)
       case list: JList[String] => list
       case _ => List()
+    }
+    this.selector = source match {
+        case "system" => None
+        case _ => projId
     }
   }
 
@@ -29,6 +36,7 @@ class DatabagDataSource(repository: DatabagRepository) extends VarDataSource {
 
 object DatabagDataSource {
   val Tags = "tags"
+  val Source = "source"
 }
 
 
