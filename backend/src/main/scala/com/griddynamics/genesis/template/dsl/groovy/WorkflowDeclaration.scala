e8b4f6f115bb3d805e31d92cf01180e860336c13
Svyatoslav Reyentenko <rsvato@gmail.com>
2012-08-26 00:29:23 +0400

    [DOC-XXX] [feature] - Workflow preconditions support

--- backend/src/main/scala/com/griddynamics/genesis/template/dsl/groovy/WorkflowDeclaration.scala
+++ backend/src/main/scala/com/griddynamics/genesis/template/dsl/groovy/WorkflowDeclaration.scala
@@ -5,6 +5,7 @@ import groovy.lang.Closure
 class WorkflowDeclaration {
     var variablesBlock : Option[Closure[Unit]] = None
     var stepsBlock : Option[Closure[Unit]] = None
+    var beforeBlock: Option[Closure[Unit]] = None
 
     def variables(variables : Closure[Unit]) {
         variablesBlock = Some(variables)
@@ -13,9 +14,17 @@ class WorkflowDeclaration {
     def steps(steps : Closure[Unit]) {
         stepsBlock = Some(steps)
     }
+
+    def require(requirements: Closure[java.util.Map[String, Closure[Boolean]]]) {
+        import collection.JavaConversions._
+        val call = mapAsScalaMap(requirements.call)
+        call.toMap.foreach(e => e._2.call())
+    }
+
 }
 
 class EnvWorkflow(val name : String,
                   val variables : List[VariableDetails],
-                  val stepsGenerator : Option[Closure[Unit]])
+                  val stepsGenerator : Option[Closure[Unit]],
+                  val complete: Option[Closure[Unit]] = None)
 
---
+++ backend/src/test/scala/com/griddynamics/genesis/service/impl/PreconditionsTests.scala
@@ -0,0 +1,34 @@
+package com.griddynamics.genesis.service.impl
+
+import com.griddynamics.genesis.template.{VersionedTemplate, ListVarDSFactory, TemplateRepository}
+import org.springframework.core.convert.support.ConversionServiceFactory
+import org.junit.{Test, Before}
+import com.griddynamics.genesis.util.IoUtil
+import com.griddynamics.genesis.repository.DatabagRepository
+import net.sf.ehcache.CacheManager
+import com.griddynamics.genesis.template.support.DatabagDataSourceFactory
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito
+import org.scalatest.junit.AssertionsForJUnit
+import com.griddynamics.genesis.service.ValidationError
+
+class PreconditionsTests extends AssertionsForJUnit with MockitoSugar {
+    val templateRepository = mock[TemplateRepository]
+    val bagRepository = mock[DatabagRepository]
+    val templateService = new GroovyTemplateService(templateRepository,
+        List(new DoNothingStepBuilderFactory), ConversionServiceFactory.createDefaultConversionService(),
+        Seq(new ListVarDSFactory, new DependentListVarDSFactory, new DatabagDataSourceFactory(bagRepository)), bagRepository, CacheManager.getInstance())
+    val body = IoUtil.streamAsString(classOf[GroovyTemplateServiceTest].getResourceAsStream("/groovy/Preconditions.genesis"))
+
+    Mockito.when(templateRepository.listSources).thenReturn(Map(VersionedTemplate("1") -> body))
+
+    @Before def setUp() {
+        CacheManager.getInstance().clearAll()
+    }
+
+    @Test
+    def testPreconditions() {
+        val createWorkflow = templateService.findTemplate(0, "Precondidions", "0.1").get.createWorkflow
+    }
+
+}
