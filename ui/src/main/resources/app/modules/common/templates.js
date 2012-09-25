ef0a8cdeac8131cfa02c3571aeb5155743890ee4
Andrey Zhuchkov <azhuchkov@griddynamics.com>
2012-09-25 20:06:39 +0400

    Revert "[RJA-733] If template contains long string, it's impossible to see it's end"
    This reverts commit 61c9f05cb5a54979de674d5ea2028c3716d2a9ce.

--- ui/src/main/resources/app/modules/common/templates.js
+++ ui/src/main/resources/app/modules/common/templates.js
@@ -48,6 +48,58 @@ function(genesis, Backbone) {
     }
   });
 
+  Templates.SourcesView = Backbone.View.extend({
+
+    initialize: function(options) {
+      this.templateName = options.templateName;
+      this.templateVersion = options.templateVersion;
+      this.projectId = options.projectId;
+    },
+
+    onClose: function() {
+      if(this.sourceCodeDialog != null) {
+        this.sourceCodeDialog.dialog('destroy').remove();
+      }
+    },
+
+    showTemplate: function (name, version) {
+      this.render(name, version);
+    },
+
+    render: function(templateName, templateVersion) {
+      var sources = new TemplateDesc({name: templateName, version: templateVersion}, {projectId: this.projectId});
+      genesis.app.trigger("page-view-loading-started");
+      var self = this;
+      $.when(sources.fetchSources())
+        .done(function() {
+          var content = _.escape(sources.get("content"));
+          if ($.browser.msie) {
+            content = content.replace(/\n/g, '<br/>').replace(/  /g, "&nbsp;");
+          }
+          self.$el.html(content);
+          if (self.sourceCodeDialog == null) {
+            self.sourceCodeDialog = self.$el.dialog({
+              resizable: true,
+              modal: true,
+              width: 900,
+              minWidth: 500,
+              minHeight: 400,
+              height: 400,
+              dialogClass: 'dialog-without-header',
+              autoOpen: false
+            });
+          }
+          genesis.app.trigger("page-view-loading-completed");
+          prettyPrint();
+          self.sourceCodeDialog.dialog("option", "title", "Template: " + templateName + " / v" + templateVersion).dialog("open");
+        })
+        .fail(function(jqXHR) {
+          genesis.app.trigger("page-view-loading-completed");
+          status.StatusPanel.error(jqXHR);
+        });
+    }
+  });
+
   Templates.TemplateModel = TemplateDesc;
   Templates.WorkflowModel = WorkflowDesc;
 
