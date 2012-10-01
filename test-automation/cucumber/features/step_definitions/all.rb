b1d7004dd043014cd1aef119b1716cd6f4f61442
Svyatoslav Reyentenko <rsvato@gmail.com>
2012-10-01 16:43:44 +0400

    [DOC-XXX] [feature] - Client removed

--- test-automation/cucumber/features/step_definitions/all.rb
+++ test-automation/cucumber/features/step_definitions/all.rb
@@ -1,5 +1,4 @@
 Given /^Genesis is running$/ do
-  @client = GenesisClient.new()
   resource "" do |r|
     response = r.get
     response.code.should eq(200), "Expected response code 200, but really got #{response.code}"
