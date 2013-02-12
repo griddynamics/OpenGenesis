0a42a3d2b3245188e7fc0df6f13953baccad719f
Rafael Bagmanov <rbagmanov@griddynamics.com>
2013-02-12 14:59:13 +0400

    [RJA-1706][feature] expose agents and database health

---
+++ backend/src/main/scala/com/griddynamics/genesis/agents/RemoteAgentHealthCheck.scala
@@ -0,0 +1,36 @@
+/**
+ *   Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
+ *   http://www.griddynamics.com
+ *
+ *   This library is free software; you can redistribute it and/or modify it under the terms of
+ *   the GNU Lesser General Public License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or any later version.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ *   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   Project:     Genesis
+ *   Description: Continuous Delivery Platform
+ */ package com.griddynamics.genesis.agents
+
+import com.yammer.metrics.core.HealthCheck
+import com.griddynamics.genesis.service.AgentsHealthService
+import com.griddynamics.genesis.api.{AgentStatus, RemoteAgent}
+
+class RemoteAgentHealthCheck(healthService: AgentsHealthService, remoteAgent: RemoteAgent, name: String) extends HealthCheck(name) {
+  def check() = {
+    val (status, stat) = healthService.checkStatus(remoteAgent)
+    status match {
+      case AgentStatus.Active => HealthCheck.Result.healthy()
+      case status => HealthCheck.Result.unhealthy(status.toString)
+    }
+  }
+}
---
+++ backend/src/main/scala/com/griddynamics/genesis/configuration/DatabaseHealthCheckContext.scala
@@ -0,0 +1,69 @@
+/**
+ *   Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
+ *   http://www.griddynamics.com
+ *
+ *   This library is free software; you can redistribute it and/or modify it under the terms of
+ *   the GNU Lesser General Public License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or any later version.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ *   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   Project:     Genesis
+ *   Description: Continuous Delivery Platform
+ */ package com.griddynamics.genesis.configuration
+
+import com.yammer.metrics.core.HealthCheck
+import java.lang.String
+import java.sql.{ResultSet, Statement, SQLException}
+import javax.sql.DataSource
+import scala.util.control.Exception._
+import org.springframework.context.annotation.Configuration
+import org.springframework.beans.factory.annotation.Autowired
+import org.apache.commons.dbcp.BasicDataSource
+import javax.annotation.PostConstruct
+import com.yammer.metrics.HealthChecks
+
+@Configuration
+class DatabaseHealthCheckContext {
+
+  @Autowired var dataSource: BasicDataSource  = _
+
+  @PostConstruct
+  def init() {
+    HealthChecks.register(new DatabaseHealthCheck(dataSource))
+  }
+}
+
+class DatabaseHealthCheck(dataSource: DataSource) extends HealthCheck("database health check") {
+  def check() = {
+    val query: String = "SELECT 1"
+    val conn = dataSource.getConnection
+
+    var stmt: Statement = null
+    var rset: ResultSet = null
+    try {
+      stmt = conn.createStatement
+      stmt.setQueryTimeout(2)
+      rset = stmt.executeQuery(query)
+      if (rset.next())
+        HealthCheck.Result.healthy()
+      else
+        HealthCheck.Result.unhealthy("Ping message did not return response")
+    } catch {
+      case e: Exception => HealthCheck.Result.unhealthy(e)
+    } finally {
+      Option(rset).map( r => ignoring(classOf[Exception]) { r.close() })
+      Option(stmt).map( s => ignoring(classOf[Exception]) { s.close() })
+      Option(conn).map( c => ignoring(classOf[Exception]) { c.close() })
+    }
+  }
+}
