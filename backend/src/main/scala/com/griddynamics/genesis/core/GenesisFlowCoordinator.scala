2dfa32b7c2e93cb6b9e15d56805fc99c03e0523d
Rafael Bagmanov <rbagmanov@griddynamics.com>
2013-03-07 15:18:03 +0400

    [RJA-xx][feature] basic workflow events  bus

--- backend/src/main/scala/com/griddynamics/genesis/core/GenesisFlowCoordinator.scala
+++ backend/src/main/scala/com/griddynamics/genesis/core/GenesisFlowCoordinator.scala
@@ -40,7 +40,7 @@ import com.griddynamics.genesis.workflow.signal.{Success, Fail}
 import com.griddynamics.genesis.plugin.GenesisStep
 import com.griddynamics.genesis.template.dsl.groovy.Reserved
 import java.util.Date
-import com.griddynamics.genesis.scheduler.JobServiceProvider
+import com.griddynamics.genesis.core.events.{EnvDestroyed, WorkflowFinished, WorkflowEventsBus}
 
 abstract class GenesisFlowCoordinator(envId: Int,
                                       projectId: Int,
@@ -113,6 +113,7 @@ abstract class GenesisFlowCoordinatorBase(val envId: Int,
         val updEnv = storeService.findEnv(env.id).get // updating optimistic lock counter
         updEnv.status = envStatus
         storeService.finishWorkflow(updEnv, workflow)
+        WorkflowEventsBus.publish(new WorkflowFinished(projectId, envId, workflow.id, workflow.status))
     }
 
     def onStepFinish(result: StepResult) = result match {
@@ -355,13 +356,13 @@ trait RegularWorkflow { this: GenesisFlowCoordinatorBase =>
     override val onFlowFinishSuccess = EnvStatus.Ready
 }
 
-trait DestroyWorkflow extends FlowCoordinator { self: GenesisFlowCoordinatorBase with JobServiceProvider =>
+trait DestroyWorkflow extends FlowCoordinator { self: GenesisFlowCoordinatorBase =>
     override val onFlowFinishSuccess = EnvStatus.Destroyed
 
     abstract override def onFlowFinish(signal: Signal) {
       super.onFlowFinish(signal)
       signal match {
-        case Success() => self.jobService.removeAllScheduledJobs(self.projectId, self.envId)
+        case Success() => WorkflowEventsBus.publish(new EnvDestroyed(self.projectId, self.envId))
         case _ => /* no nothing */
       }
     }
