5f29a6a82a7cad2472656523bc478806daad704d
Svyatoslav Reyentenko <rsvato@gmail.com>
2013-04-25 16:04:33 +0400

    Phases dsl

---
+++ backend/src/main/scala/com/griddynamics/genesis/template/dsl/groovy/transformations/PhaseContainerASTTransformation.scala
@@ -0,0 +1,116 @@
+package com.griddynamics.genesis.template.dsl.groovy.transformations
+
+import org.codehaus.groovy.transform.{GroovyASTTransformation, ASTTransformation}
+import com.griddynamics.genesis.util.Logging
+import org.codehaus.groovy.ast.{CodeVisitorSupport, ASTNode}
+import org.codehaus.groovy.control.{CompilePhase, SourceUnit}
+import org.codehaus.groovy.ast.stmt.{Statement, BlockStatement}
+import org.codehaus.groovy.ast.expr._
+import java.util
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
+
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+class PhaseContainerASTTransformation extends ASTTransformation with Logging{
+  def visit(nodes: Array[ASTNode], source: SourceUnit) {
+    log.debug("In phase transformation")
+    val methods = source.getAST.getStatementBlock.getStatements
+    val it = methods.iterator().next()
+    it.visit(new PhaseEraser)
+  }
+}
+
+class PhaseEraser extends CodeVisitorSupport with Logging {
+   override def visitMethodCallExpression(call: MethodCallExpression) {
+     if (call.getMethodAsString == "steps") {
+       log.debug("Found steps call")
+       val expr = call.getArguments.transformExpression(new PhaseTransformer)
+       call.setArguments(expr)
+     } else {
+       call.getArguments.visit(this)
+     }
+   }
+}
+
+class PhaseTransformer extends ExpressionTransformer with Logging{
+  def transform(expression: Expression): Expression = {
+    if (expression.isInstanceOf[ClosureExpression]) {
+      val closure: ClosureExpression = expression.asInstanceOf[ClosureExpression]
+      val phaseCall = closure.getCode
+       val phasesCalls = new PhasesSearch()
+       phaseCall.visit(phasesCalls)
+       log.debug(s"Found ${phasesCalls.found} of phases container at examined expression")
+       log.debug(s"Collected: ${phasesCalls.phases.toList}")
+       if (phasesCalls.found == 0) {
+         expression
+       } else {
+         val statements = phasesCalls.phases.toList.collect({case p => p.toStatements}).flatten
+         closure.setCode(new BlockStatement(statements.toArray, closure.getVariableScope))
+         closure
+       }
+    } else {
+      expression
+    }
+  }
+}
+
+class PhasesSearch extends CodeVisitorSupport with Logging {
+  var found: Int = 0
+  val phases: ListBuffer[PhaseDefinition] = ListBuffer[PhaseDefinition]()
+  override def visitMethodCallExpression(call: MethodCallExpression) {
+    if (call.getMethodAsString == "phase") {
+      found += 1
+      val arguments: Expression = call.getArguments
+      val phaseDef = arguments match {
+        case a: ArgumentListExpression => extractPhaseDefinition(a)
+        case _ => None
+      }
+      phaseDef.map(phases.append(_))
+    }
+  }
+
+  def extractPhaseDefinition(list: ArgumentListExpression): Option[PhaseDefinition] = {
+    def hasName(name: String, expr: Expression) = {
+      expr.isInstanceOf[ConstantExpression] && expr.asInstanceOf[ConstantExpression].getValue == name
+    }
+    def hasType(klass: Class[_], expr: Expression) = {
+      expr.getClass == klass
+    }
+    def readPhaseArguments(expr: MapExpression) : Option[PhaseDefinition] = {
+       if (expr.getMapEntryExpressions.size() > 0) {
+         import scala.collection.JavaConversions._
+         val nameExpr = expr.getMapEntryExpressions.find(p => hasName("name", p.getKeyExpression))
+         val afterExpr = expr.getMapEntryExpressions.find(p => hasName("after", p.getKeyExpression) && hasType(classOf[ListExpression], p.getValueExpression))
+         (nameExpr, afterExpr) match {
+           case (None, _) => None
+           case (Some(name), None) => Some(new PhaseDefinition(name.getValueExpression))
+           case (Some(name), Some(lst)) => Some(new PhaseDefinition(name.getValueExpression,
+             lst.getValueExpression.asInstanceOf[ListExpression].getExpressions.toList))
+         }
+       } else {
+         None
+       }
+    }
+    val expressions: util.List[Expression] = list.getExpressions
+    if (expressions.size() < 2) {
+      None
+    } else {
+      val firstExpression: Expression = expressions.get(0)
+      val definition = (firstExpression match {
+        case map: MapExpression => readPhaseArguments(map)
+        case _ => None
+      }).flatMap(phase => {
+         expressions.get(1).asInstanceOf[ClosureExpression].getCode match {
+           case block: BlockStatement => Some(phase.copy(steps = block.getStatements.toList))
+           case _ => None
+         }
+      })
+      definition
+    }
+  }
+}
+
+
+case class PhaseDefinition(name: Expression, preceding: List[Expression] = List(), steps: List[Statement] = List()) {
+  def toStatements: List[Statement] = steps
+}
---
+++ backend/src/test/scala/com/griddynamics/genesis/service/impl/PhaseContainerTest.scala
@@ -0,0 +1,38 @@
+package com.griddynamics.genesis.service.impl
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.mock.MockitoSugar
+import org.springframework.core.convert.support.DefaultConversionService
+import com.griddynamics.genesis.template.{VersionedTemplate, ListVarDSFactory}
+import com.griddynamics.genesis.template.support.DatabagDataSourceFactory
+import com.griddynamics.genesis.cache.NullCacheManager
+import com.griddynamics.genesis.util.{Logging, IoUtil}
+import org.mockito.{Matchers, Mockito}
+import org.mockito.Mockito._
+import scala.Some
+import com.griddynamics.genesis.template.VersionedTemplate
+import com.griddynamics.genesis.api
+import com.griddynamics.genesis.plugin.StepBuilder
+import org.junit.Test
+
+
+class PhaseContainerTest extends AssertionsForJUnit with MockitoSugar with DSLTestUniverse with Logging {
+  val templateService = new GroovyTemplateService(templateRepoService,
+    List(new DoNothingStepBuilderFactory), new DefaultConversionService,
+    Seq(new ListVarDSFactory, new DependentListVarDSFactory,
+      new DatabagDataSourceFactory(databagRepository)), databagRepository, configService, NullCacheManager)
+
+  val body = IoUtil.streamAsString(classOf[GroovyTemplateServiceTest].getResourceAsStream("/groovy/PhaseContainer.genesis"))
+
+  Mockito.when(templateRepository.listSources()).thenReturn(Map(VersionedTemplate("1") -> body))
+  when(configService.get(Matchers.any(), Matchers.any())).thenReturn(Some(new api.Configuration(Some(0), "", 0, None, Map())))
+
+  @Test
+  def testPhaseApplied() {
+    val createWorkflow = templateService.findTemplate(0, "PhaseContainer", "0.1", 0).get.createWorkflow
+    val steps = createWorkflow.embody(Map())
+    expectResult(3)(steps.regular.size)
+    val firstStep: StepBuilder = steps(0)
+    expectResult("initial")(firstStep.getPhase)
+  }
+}
